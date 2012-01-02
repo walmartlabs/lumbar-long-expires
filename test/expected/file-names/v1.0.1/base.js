45e735a273fd295d2214cb6e1b84f56e2fe9216f
kpdecker <kpdecker@gmail.com>
2012-01-01 22:48:38 -0600

    Basic file names test

---
+++ test/expected/file-names/v1.0.1/base.js
@@ -0,0 +1,8 @@
+var base;
+base = (function() {
+var module = {exports: {}};
+var exports = module.exports;
+var bar;
+;;
+return module.exports;
+}).call(this);
--- test/lumbar-long-expires.js
+++ test/lumbar-long-expires.js
@@ -1,8 +1,11 @@
 var assert = require('assert'),
+    lib = require('../node_modules/lumbar/test/lib'),
     longExpires = require('../lib/lumbar-long-expires');
 
+exports['file-names'] = lib.runTest('test/artifacts/file-names.json', 'test/expected/file-names', {plugins: [longExpires]});
+
 exports['grep'] = function(done) {
-  longExpires.generateToken('grep "^v" test/artifacts/grep.txt', function(err, token) {
+  longExpires.generateToken('grep "^v" grep.txt', function(err, token) {
       if (err) {
         throw err;
       }
@@ -24,7 +27,7 @@ exports['git'] = function(done) {
 };
 
 exports['pipe'] = function(done) {
-  longExpires.generateToken('cat test/artifacts/grep.txt | grep "^v"', function(err, token) {
+  longExpires.generateToken('cat grep.txt | grep "^v"', function(err, token) {
       if (err) {
         throw err;
       }
