936f76efe41c9627709d114770aa6cd4139e2230
kpdecker <kpdecker@gmail.com>
2012-01-02 01:12:02 -0600

    Move expires token after the platform path

--- test/expected/module-map/web/v1.0.1/base.js
+++ test/expected/module-map/web/v1.0.1/base.js
@@ -5,6 +5,6 @@ var exports = module.exports;
 var bar;
 ;;
 /* lumbar module map */
-App.moduleMap({"modules":{"base":{"css":{"href":"base.css"},"js":"base.js"},"home":{"js":"home.js"}},"routes":{"home":"home"}}, 'foo/v1.0.1/web/');
+App.moduleMap({"modules":{"base":{"css":{"href":"base.css"},"js":"base.js"},"home":{"js":"home.js"}},"routes":{"home":"home"}}, 'foo/web/v1.0.1/');
 return module.exports;
 }).call(this);
