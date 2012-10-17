efd799f3c66fe361899bdab611da447baa86c225
kpdecker <kpdecker@gmail.com>
2012-10-17 09:43:18 -0500

    Remove scope plugin from tests

--- test/expected/index-update/web/v1.0.1/base.js
+++ test/expected/index-update/web/v1.0.1/base.js
@@ -1,8 +1,1 @@
-var base;
-base = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var bar;
-;;
-return module.exports;
-}).call(this);
--- test/expected/module-map/web/v1.0.1/base.js
+++ test/expected/module-map/web/v1.0.1/base.js
@@ -1,10 +1,4 @@
-var base;
-base = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var bar;
 ;;
 /* lumbar module map */
 App.moduleMap({"modules":{"base":{"css":{"href":"base.css"},"js":"base.js"},"home":{"js":"home.js"}},"routes":{"home":"home"}});
-return module.exports;
-}).call(this);
